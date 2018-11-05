c2319f1ae33f445a71603694ed69f6457ef6b035
inkh <jhwe92@gmail.com>
2018-11-04 21:19:28 -0800

    added dbcontext and models

---
+++ WebAPI/WebAPI/Data/ToDoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class ToDoDbContext : DbContext
+    {
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<ToDo> ToDos { get; set; }
+        DbSet<ToDoList> ToDoList { get; set; }
+    }
+}
