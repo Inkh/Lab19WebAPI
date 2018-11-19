803089ef9437385d660b6c114e8024d857bc8fc6
inkh <jhwe92@gmail.com>
2018-11-18 17:59:41 -0800

    completed TODO testing

--- WebAPI/ToDoAPITest/UnitTest1.cs
+++ WebAPI/ToDoAPITest/UnitTest1.cs
@@ -1,6 +1,8 @@
 using System;
 using Xunit;
 using WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
 
 namespace ToDoAPITest
 {
@@ -85,6 +87,107 @@ namespace ToDoAPITest
 
             myList.Name = "another name";
             Assert.Equal("another name", myList.Name);
+        }
+
+        /// <summary>
+        /// Tests creation on ToDo table
+        /// </summary>
+        [Fact]
+        public async void ToDoCreateTest()
+        {
+            DbContextOptions<ToDoDbContext> options =
+                new DbContextOptionsBuilder<ToDoDbContext>()
+                .UseInMemoryDatabase("todoCreate")
+                .Options;
+
+            using (ToDoDbContext context = new ToDoDbContext(options))
+            {
+
+                //CREATE
+                //Arrange
+                ToDo taskOne = new ToDo();
+                taskOne.Name = "Finish homework";
+                taskOne.Completed = false;
+
+                context.ToDos.Add(taskOne);
+                context.SaveChanges();
+
+                //READ
+                var myTask = await context.ToDos.FirstOrDefaultAsync(t => t.Name == taskOne.Name);
+
+                Assert.Equal("Finish homework", myTask.Name);
+            }
+        }
+
+        /// <summary>
+        /// Tests updating on ToDo table
+        /// </summary>
+        [Fact]
+        public async void ToDoUpdateTest()
+        {
+            DbContextOptions<ToDoDbContext> options =
+                new DbContextOptionsBuilder<ToDoDbContext>()
+                .UseInMemoryDatabase("todoUpdate")
+                .Options;
+
+            using (ToDoDbContext context = new ToDoDbContext(options))
+            {
+
+                //CREATE
+                //Arrange
+                ToDo taskOne = new ToDo();
+                taskOne.Name = "Finish homework";
+                taskOne.Completed = false;
+
+                context.ToDos.Add(taskOne);
+                context.SaveChanges();
+
+                //READ
+                var myTask = await context.ToDos.FirstOrDefaultAsync(t => t.Name == taskOne.Name);
+
+                myTask.Name = "New Name";
+                context.Update(myTask);
+                context.SaveChanges();
+
+                var newTask = await context.ToDos.FirstOrDefaultAsync(t => t.Name == myTask.Name);
+
+                Assert.Equal("New Name", newTask.Name);
+            }
+        }
+
+        /// <summary>
+        /// Tests deletion on todo table
+        /// </summary>
+        [Fact]
+        public async void ToDoDeleteTest()
+        {
+            DbContextOptions<ToDoDbContext> options =
+                new DbContextOptionsBuilder<ToDoDbContext>()
+                .UseInMemoryDatabase("todoDelete")
+                .Options;
+
+            using (ToDoDbContext context = new ToDoDbContext(options))
+            {
+
+                //CREATE
+                //Arrange
+                ToDo taskOne = new ToDo();
+                taskOne.Name = "Finish homework";
+                taskOne.Completed = false;
+
+                context.ToDos.Add(taskOne);
+                context.SaveChanges();
+
+                //READ
+                var myTask = await context.ToDos.FirstOrDefaultAsync(t => t.Name == taskOne.Name);
+
+                context.ToDos.Remove(myTask);
+                context.SaveChanges();
+
+                var deletedTask = await context.ToDos.FirstOrDefaultAsync(t => t.Name == myTask.Name);
+
+                Assert.True(deletedTask == null);
+            }
         }
     }
 }
